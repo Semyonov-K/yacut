1d1133874d6c444d2d30bc978793b63023380c8c
unknown <fixer20112@mail.com>
2023-04-21 19:58:56 +0300

    second try

--- yacut/api_views.py
+++ yacut/api_views.py
@@ -1,3 +1,4 @@
+from http import HTTPStatus
 from re import match
 
 from flask import jsonify, request
@@ -8,34 +9,41 @@ from .models import URLMap
 from .views import get_unique_short_id
 
 
+def validation_data(data):
+    if data is None:
+        raise InvalidAPIUsage('Отсутствует тело запроса')
+    if 'url' not in data:
+        raise InvalidAPIUsage('"url" является обязательным полем!')
+    if not match(
+            r'^[a-z]+://[^\/\?:]+(:[0-9]+)?(\/.*?)?(\?.*)?$', data['url']):
+        raise InvalidAPIUsage('Указан недопустимый URL')
+    if data.get('custom_id'):
+        if URLMap.query.filter_by(short=data['custom_id']).first() is not None:
+            raise InvalidAPIUsage(f'Имя "{data["custom_id"]}" уже занято.')
+        if not match(r'^[A-Za-z0-9]{1,16}$', data['custom_id']):
+            raise InvalidAPIUsage(
+                'Указано недопустимое имя для короткой ссылки',
+                HTTPStatus.BAD_REQUEST
+            )
+
+
 @app.route('/api/id/<string:short_id>/')
 def get_url(short_id):
     url_map = URLMap.query.filter_by(short=short_id).first()
     if not url_map:
         raise InvalidAPIUsage(
-            'Указанный id не найден', 404)
+            'Указанный id не найден', HTTPStatus.NOT_FOUND)
     return jsonify({'url': url_map.original})
 
 
 @app.route('/api/id/', methods=['POST'])
 def create_url():
     data = request.get_json(silent=True)
-    if not data:
-        raise InvalidAPIUsage('Отсутствует тело запроса')
+    validation_data(data)
     if not data.get('custom_id'):
         data['custom_id'] = get_unique_short_id()
-    if 'url' not in data:
-        raise InvalidAPIUsage('"url" является обязательным полем!')
-    if URLMap.query.filter_by(short=data['custom_id']).first() is not None:
-        raise InvalidAPIUsage(f'Имя "{data["custom_id"]}" уже занято.')
-    if not match(
-            r'^[a-z]+://[^\/\?:]+(:[0-9]+)?(\/.*?)?(\?.*)?$', data['url']):
-        raise InvalidAPIUsage('Указан недопустимый URL')
-    if not match(r'^[A-Za-z0-9]{1,16}$', data['custom_id']):
-        raise InvalidAPIUsage(
-            'Указано недопустимое имя для короткой ссылки', 400)
     urlmap = URLMap()
     urlmap.from_dict(data)
     db.session.add(urlmap)
     db.session.commit()
-    return jsonify(urlmap.to_dict()), 201
+    return jsonify(urlmap.to_dict()), HTTPStatus.CREATED
